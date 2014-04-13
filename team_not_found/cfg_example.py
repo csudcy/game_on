4fc1aca15d60755d57fc7aa924747476abe1e081
Nicholas Lee <csudcy@gmail.com>
2014-04-13 23:35:14 +0100

    Removed more of my crap from cfg_example

--- team_not_found/cfg_example.py
+++ team_not_found/cfg_example.py
@@ -1,7 +1,5 @@
 import os
 
-from team_not_found import platform_helper
-
 
 config = {
     'db': {
@@ -14,7 +12,9 @@ config = {
     'cherrypy': {
         'host': '127.0.0.1',
         'port': 8080,
-        'sock': '/home/tnfuser/team_not_found/dynamic/socks/tnf.{pid}.sock',
+        'sock': '/home/tnfuser/team_not_found/dynamic/socks/tnf.{pid}.sock'.format(
+            pid = os.getpid()
+        ),
         'static_timeout_hours': 0,
     },
     'logging': {
@@ -43,15 +43,3 @@ config = {
     'root_url': 'https://yourserver.com/path/to/splash_page/',
     'secret': 'TeamNotFoundIsTotallyAwesome!!1!'
 }
-
-if platform_helper.WIN32:
-    config['db']['user'] = 'postgres'
-    config['db']['password'] = 'postgres'
-    config['cherrypy']['sock'] = 'C:/dev/team_not_found/dynamic/socks/tnf.{pid}.sock'
-    config['logging']['log_folder'] = 'C:/dev/team_not_found/dynamic/logs/'
-    config['team']['folder'] = 'C:/dev/team_not_found/dynamic/teams/'
-    config['match']['folder'] = 'C:/dev/team_not_found/dynamic/matches/'
-
-config['cherrypy']['sock'] = config['cherrypy']['sock'].format(
-    pid = os.getpid()
-)
